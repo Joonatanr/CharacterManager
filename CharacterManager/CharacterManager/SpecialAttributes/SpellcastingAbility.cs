62d68749e2196daac5486ff48d2735a2649afd41
Joonatan@work <joonatanrenel@gmail.com>
2020-02-05 12:32:54 +0200

    Added an omitted file.

---
+++ CharacterManager/CharacterManager/SpecialAttributes/SpellcastingAbility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager.SpecialAttributes
+{
+    [Serializable]
+    public class SpellcastingAbility : PlayerAbility
+    {
+        /* This is basically a container class for the spellcasting ability of a PC. */
+        public string SpellCastingAttribute; /* STR, WIS, DEX, CON, CHA or INT */
+
+        public SpellcastingAbility()
+        {
+            this.AttributeName = "Spellcasting";
+            this.IsToggle = false;
+        }
+    }
+}
