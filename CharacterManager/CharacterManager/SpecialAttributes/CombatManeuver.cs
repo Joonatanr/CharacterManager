7c1f7d7aab72e86e58de9e376170bf3a3f23ebe9
Joonatan@work <joonatanrenel@gmail.com>
2022-08-31 08:43:30 +0300

    Added file that was missing...

---
+++ CharacterManager/CharacterManager/SpecialAttributes/CombatManeuver.cs
@@ -0,0 +1,17 @@
+﻿using CharacterManager.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager.SpecialAttributes
+{
+    [Serializable]
+    public class CombatManeuver : PlayerBaseItem
+    {
+        public string ManeuverName;
+
+        public override string ItemName { get => ManeuverName; set => ManeuverName = value; }
+    }
+}
